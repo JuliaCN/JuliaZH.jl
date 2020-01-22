93a8f8841995b809eb8e6781c53fb9a0a05aeb21
Rogerluo <rogerluo.rl18@gmail.com>
2020-01-22 10:16:24 -0500

    更新版本 & 添加compat (#87)
    * bump version & add compat
    
    * rm old config
    
    * fix parathesis
    
    * rm time stamp
    
    * fix edit
    
    * fix translation
    
    * add latex back

--- contrib/HTMLWriter.jl
+++ contrib/HTMLWriter.jl
@@ -1,44 +1,30 @@
 # override Documenter.jl/src/Writers/HTMLWriter.jl
-
+using Dates
 import Documenter: Documents, Documenter, Writers, Utilities
 import Documenter.Utilities.DOM: DOM, Tag, @tags
 import Documenter.Writers.HTMLWriter:
     getpage,
     render_head,
-    render_navmenu,
     render_article,
     open_output,
     mdconvert,
     pagetitle,
     navhref,
-    render_topbar,
-    domify
+    domify,
+    render_settings,
+    THEMES
 
-#=
-  1. 修改 html 的 lang 属性为 zh-cn
-=#
-function Documenter.Writers.HTMLWriter.render_page(ctx, navnode)
-    @tags html body
+const zh_CN_numbers = ["一", "二", "三", "四", "五", "六",
+    "七", "八", "九", "十", "十一", "十二"]
+const zh_CN_months = zh_CN_numbers[1:12] .* "月"
 
-    page = getpage(ctx, navnode)
+const zh_CN_months_abbrev = zh_CN_months
+const zh_CN_days = [
+    "周" .* zh_CN_numbers[1:6]
+    "周日"
+]
 
-    head = render_head(ctx, navnode)
-    navmenu = render_navmenu(ctx, navnode)
-    article = render_article(ctx, navnode)
-
-    htmldoc = DOM.HTMLDocument(
-        # 修改 lang tag
-        html[:lang=>"zh-cn"](
-            head,
-            body(navmenu, article)
-        )
-    )
-
-    open_output(ctx, navnode) do io
-        print(io, htmldoc)
-    end
-end
-
+Dates.LOCALES["zh_CN"] = Dates.DateLocale(zh_CN_months, zh_CN_months_abbrev, zh_CN_days, [""])
 
 #=
   1. 修改编辑翻译提示语
@@ -52,9 +38,6 @@ end
 
 const t_Edit_on_xx = " 完善 Transifex 上的翻译" # 开头加空格，避免与 icon 靠太近
 const t_Icon = "\uf0ac" # fa-globe
-
-const t_Previous = "上一篇"
-const t_Next     = "下一篇"
 
 # 生成对应文件在 Transifex 上的翻译地址
 function transifex_url(rel_path)
@@ -80,45 +63,95 @@ function transifex_url(rel_path)
     "https://www.transifex.com/juliacn/$parent_fold-zh_cn/translate/#zh_CN/$page_name"
 end
 
+
+
 # workaround on Documenter/#977
-function Documenter.Writers.HTMLWriter.render_article(ctx, navnode)
-    @tags article header footer nav ul li hr span a
+function Documenter.Writers.HTMLWriter.render_navbar(ctx, navnode, edit_page_link::Bool)
+    @tags div header nav ul li a span
 
-    header_links = map(Documents.navpath(navnode)) do nn
+    # The breadcrumb (navigation links on top)
+    navpath = Documents.navpath(navnode)
+    header_links = map(navpath) do nn
         title = mdconvert(pagetitle(ctx, nn); droplinks=true)
-        nn.page === nothing ? li(title) : li(a[:href => navhref(ctx, nn, navnode)](title))
+        nn.page === nothing ? li(a[".is-disabled"](title)) : li(a[:href => navhref(ctx, nn, navnode)](title))
     end
+    header_links[end] = header_links[end][".is-active"]
+    breadcrumb = nav[".breadcrumb"](
+        ul[".is-hidden-mobile"](header_links),
+        ul[".is-hidden-tablet"](header_links[end]) # when on mobile, we only show the page title, basically
+    )
 
-    topnav = nav(ul(header_links))
+    # The "Edit on GitHub" links and the hamburger to open the sidebar (on mobile) float right
+    navbar_right = div[".docs-right"]
 
     # Set the logo and name for the "Edit on.." button.
-    host_type = Utilities.repo_host_from_url(ctx.doc.user.repo)
+    if edit_page_link && (ctx.settings.edit_link !== nothing) && !ctx.settings.disable_git
+        host_type = Utilities.repo_host_from_url(ctx.doc.user.repo)
+        logo = t_Icon
 
-    if !ctx.settings.disable_git # 1.3: 改 URL
+        pageurl = get(getpage(ctx, navnode).globals.meta, :EditURL, getpage(ctx, navnode).source)
+        edit_branch = isa(ctx.settings.edit_link, String) ? ctx.settings.edit_link : nothing
+        
+        # 1.3: 改 URL
         url = transifex_url(getpage(ctx, navnode).source)
         if url !== nothing
-            # 1.1 & 1.2: 修改编辑翻译 icon/提示语
-            push!(topnav.nodes, a[".edit-page", :href => url](span[".fa"](t_Icon), t_Edit_on_xx))
+            title = t_Edit_on_xx
+            push!(navbar_right.nodes,
+                a[".docs-edit-link", :href => url, :title => title](
+                    span[".docs-icon.fab"](logo),
+                    span[".docs-label.is-hidden-touch"](title)
+                )
+            )
         end
     end
-    art_header = header(topnav, hr(), render_topbar(ctx, navnode))
 
-    # build the footer with nav links
-    art_footer = footer(hr())
-    if navnode.prev !== nothing
-        direction = span[".direction"](t_Previous) # 2.1: 修改翻页提示语
-        title = span[".title"](mdconvert(pagetitle(ctx, navnode.prev); droplinks=true))
-        link = a[".previous", :href => navhref(ctx, navnode.prev, navnode)](direction, title)
-        push!(art_footer.nodes, link)
-    end
+    # Settings cog
+    push!(navbar_right.nodes, a[
+        "#documenter-settings-button.docs-settings-button.fas.fa-cog",
+        :href => "#", :title => "设置",
+    ])
 
-    if navnode.next !== nothing
-        direction = span[".direction"](t_Next) # 2.1: 修改翻页提示语
-        title = span[".title"](mdconvert(pagetitle(ctx, navnode.next); droplinks=true))
-        link = a[".next", :href => navhref(ctx, navnode.next, navnode)](direction, title)
-        push!(art_footer.nodes, link)
-    end
+    # Hamburger on mobile
+    push!(navbar_right.nodes, a[
+        "#documenter-sidebar-button.docs-sidebar-button.fa.fa-bars.is-hidden-desktop",
+        :href => "#"
+    ])
 
-    pagenodes = domify(ctx, navnode)
-    article["#docs"](art_header, pagenodes, art_footer)
+    # Construct the main <header> node that should be the first element in div.docs-main
+    header[".docs-navbar"](breadcrumb, navbar_right)
+end
+
+function render_settings(ctx)
+    @tags div header section footer p button hr span select option label a
+
+    theme_selector = p(
+        label[".label"]("选择主题"),
+        div[".select"](
+            select["#documenter-themepicker"](option[:value=>theme](theme) for theme in THEMES)
+        )
+    )
+
+    now_full = Dates.format(now(), "Y U d E HH:MM"; locale="zh_CN")
+    now_short = Dates.format(now(), "Y U d E"; locale="zh_CN")
+    buildinfo = p(
+        "本文档在",
+        span[".colophon-date", :title => now_full](now_short),
+        "由",
+        a[:href => "https://github.com/JuliaDocs/Documenter.jl"]("Documenter.jl"),
+        "使用$(Base.VERSION)版本的Julia生成。"
+    )
+
+    div["#documenter-settings.modal"](
+        div[".modal-background"],
+        div[".modal-card"](
+            header[".modal-card-head"](
+                p[".modal-card-title"]("设置"),
+                button[".delete"]()
+            ),
+            section[".modal-card-body"](
+                theme_selector, hr(), buildinfo
+            ),
+            footer[".modal-card-foot"]()
+        )
+    )
 end
