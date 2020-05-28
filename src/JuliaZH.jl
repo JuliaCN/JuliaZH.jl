c891428ecd6860105eb2398531b40e2a527327d8
Roger-luo <rogerluo.rl18@gmail.com>
2020-05-27 23:57:20 -0400

    set pkg server automatically

--- src/JuliaZH.jl
+++ src/JuliaZH.jl
@@ -4,8 +4,20 @@ import Base.Docs: DocStr
 
 const keywords = Dict{Symbol, DocStr}()
 
+const mirrors = Dict{String, String}()
+
+mirrors["BFSU"] = "https://mirrors.bfsu.edu.cn/julia/static"
+mirrors["default"] = "https://pkg.julialang.org"
+
+function set_mirror(mirror_name="BFSU")
+    ENV["JULIA_PKG_SERVER"] = mirrors[mirror_name]
+    return
+end
+
 function __init__()
+    # set to Chinese env by default
     ENV["REPL_LOCALE"] = "zh_CN"
+    set_mirror("BFSU")
     if ccall(:jl_generating_output, Cint, ()) == 0
         include(joinpath(@__DIR__, "i18n_patch.jl"))
         include(joinpath(@__DIR__, "basedocs.jl"))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,7 @@
 using JuliaZH
 using Test
+
+@testset "check ENV" begin
+    @test ENV["REPL_LOCALE"] == "zh_CN"
+    @test ENV["JULIA_PKG_SERVER"] == "https://mirrors.bfsu.edu.cn/julia/static"
+end
