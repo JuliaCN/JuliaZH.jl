7bca1c9ac9cb76cc14422ef2cce55dda5a5c76c0
Johnny Chen <johnnychen94@hotmail.com>
2021-10-08 12:51:38 +0800

    use PkgServerClient to choose the best mirror server as pkg server (#121)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,11 @@
-using JuliaZH
 using Test
+
+ENV["JULIA_PKG_SERVER"] = ""
+using JuliaZH
+@test !isempty(ENV["JULIA_PKG_SERVER"])
 
 @testset "check ENV" begin
     @test ENV["REPL_LOCALE"] == "zh_CN"
-    @test ENV["JULIA_PKG_SERVER"] == "https://mirrors.bfsu.edu.cn/julia/static"
+    JuliaZH.set_mirror("BFSU")
+    @test ENV["JULIA_PKG_SERVER"] == "https://mirrors.bfsu.edu.cn/julia"
 end
