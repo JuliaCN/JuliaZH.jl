569f03547aa31d5c9c2d0bdab306929908e348d1
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-12 21:15:46 +0800

    doc: 删除过时的新手指引链接

--- src/basedocs.jl
+++ src/basedocs.jl
@@ -9,10 +9,6 @@ import Base.BaseDocs: @kw_str
 
     https://cn.julialang.org
 
-新手请参考中文 discourse 上的新手指引
-
-    https://discourse.juliacn.com/t/topic/159
-
 输入 `?`， 然后输入你想要查看帮助文档的函数或者宏名称就可以查看它们的文档。例如 `?cos`, 或者 `?@time` 然后按回车键即可。
 
 在 REPL 中输入 `ENV["REPL_LOCALE"]=""` 将恢复英文模式。再次回到中文模式请输入 `ENV["REPL_LOCALE"]="zh_CN"`。
